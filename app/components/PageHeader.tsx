222ff735db40939f3c3565e3f6ad9029a8dc6bd3
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-11 11:47:45 +0330

    Update PageHeader styling

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function PageHeader() {
   return (
-    <header className="w-full flex justify-start items-center p-4 relative z-10">
+    <header className="w-full flex justify-start items-center p-8 px-12 relative z-10">
       <span>Parallel Universe Archive</span>
       <nav className="ms-auto flex justify-start items-center gap-2">
         <Link href="/">Recommendations</Link>
