3b91042667a96a94131ec7817f747040bd445167
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-11 12:39:12 +0330

    Add framer-motion animation to GameCard component and use AnimatePresence in GameList

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { motion } from "framer-motion";
 
 export default function GameCard({
+  id,
   name,
   cover_image,
   modalCall,
 }: {
+  id: string;
   name: string;
   cover_image: string;
   modalCall: () => void;
@@ -14,18 +17,30 @@ export default function GameCard({
   }
 
   return (
-    <div
-      onClick={handleClick}
-      className="col-span-1 row-span-1 card rounded-lg relative overflow-hidden cursor-pointer"
+    <motion.div
+      key={id}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.5 }}
+      transition={{
+        duration: 0.8,
+        delay: 0,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
     >
-      <div className="absolute inset-4 flex justify-center items-center text-center">
-        <span className="text-slate-300 text-sm">{name}</span>
-      </div>
-
       <div
-        className="w-full rounded-lg aspect-w-6 aspect-h-9 bg-cover bg-center"
-        style={{ backgroundImage: `url(${cover_image})` }}
-      ></div>
-    </div>
+        onClick={handleClick}
+        className="col-span-1 row-span-1 card rounded-lg relative overflow-hidden cursor-pointer"
+      >
+        <div className="absolute inset-4 flex justify-center items-center text-center">
+          <span className="text-slate-300 text-sm">{name}</span>
+        </div>
+
+        <div
+          className="w-full rounded-lg aspect-w-6 aspect-h-9 bg-cover bg-center"
+          style={{ backgroundImage: `url(${cover_image})` }}
+        ></div>
+      </div>
+    </motion.div>
   );
 }
--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence } from "framer-motion";
 import type { Game } from "@/types/supabase.types";
 
 import GameCard from "@/app/components/GameCard";
@@ -8,18 +9,21 @@ import GameListEmpty from "@/app/components/GameListEmpty";
 export default function GameList({ games }: { games: Game[] }) {
   return (
     <div className="w-full grid grid-cols-8 gap-2">
-      {games ? (
-        games.map((game) => (
-          <GameCard
-            key={game.id}
-            name={game.name}
-            cover_image={game.cover_image || ""}
-            modalCall={() => console.log(game.name)}
-          />
-        ))
-      ) : (
-        <GameListEmpty />
-      )}
+      <AnimatePresence>
+        {games ? (
+          games.map((game) => (
+            <GameCard
+              id={game.id}
+              key={game.id}
+              name={game.name}
+              cover_image={game.cover_image || ""}
+              modalCall={() => console.log(game.name)}
+            />
+          ))
+        ) : (
+          <GameListEmpty />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
