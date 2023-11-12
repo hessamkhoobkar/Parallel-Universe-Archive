c33c82a492f8f3deac02968262ac19c6dd513b39
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-13 00:19:06 +0330

    Add logo to PageHeader component

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/app/assets/images/logo.png";
 
 export default function PageHeader() {
   return (
     <header className="w-full flex justify-start items-center p-8 px-12 relative z-10">
-      <span>Parallel Universe Archive</span>
+      <Image src={logo} alt="" width={200} height={50} />
       <nav className="ms-auto flex justify-start items-center gap-2">
         <Link href="/">Recommendations</Link>
         <Link href="/queue">Queue to play </Link>
