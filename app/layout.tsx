56cfb8f10335adebf6c77a79fd49ed1b9f0af5ff
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-12 23:45:11 +0330

    Add MonaSans font files and update layout to use them

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import PageHeader from "@/app/components/PageHeader";
 
-const inter = Inter({ subsets: ["latin"] });
+const monasans = localFont({
+  src: [
+    {
+      path: "./assets/fonts/MonaSans-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/MonaSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Parallel Universe Archive",
@@ -17,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={monasans.className}>
         <div className="absolute inset-0 bg-no-repeat bg-auto bg-right-top body-gr-bg z-0"></div>
         <PageHeader />
         <main className="flex min-h-screen flex-col items-start justify-start px-12 py-12 relative z-10">
