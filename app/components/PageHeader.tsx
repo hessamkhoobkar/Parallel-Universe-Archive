c01fc6e7eaf3c4da00a2e3e0cf9504285cd4a8a0
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-04 09:54:20 +0330

    Refactor PageHeader component to include navigation links

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -1,8 +1,13 @@
-// import Link from "next/link";
+"use client";
+
+import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import logo from "@/app/assets/images/logo.png";
 
 export default function PageHeader() {
+  const pathname = usePathname();
+
   return (
     <header className="w-full flex justify-start items-center p-4 lg:py-8 lg:px-12 relative z-10">
       <Image
@@ -13,18 +18,32 @@ export default function PageHeader() {
         className="w-auto h-auto"
       />
       <nav className="ms-auto p-1 lg:flex justify-start items-center gap-2 nav-group rounded-lg shadow border border-slate-900 border-opacity-20 hidden">
-        <button
-          type="button"
-          className={`px-4 py-2 rounded-lg text-white hover:text-gray-200 transition-all duration-200 ease-in-out nav-item active`}
-        >
-          Recommendations
-        </button>
-        <button
-          type="button"
-          className={`px-4 py-2 rounded-lg text-white text-opacity-30 transition-all duration-200 ease-in-out nav-item cursor-not-allowed`}
-        >
-          Queue to play
-        </button>
+        <Link href="/">
+          <button
+            type="button"
+            className={`px-4 py-2 rounded-lg text-white  transition-all duration-200 ease-in-out nav-item 
+            ${
+              pathname === "/"
+                ? "active"
+                : "text-opacity-30 hover:text-opacity-60"
+            }`}
+          >
+            Recommendations
+          </button>
+        </Link>
+        <Link href="/queue">
+          <button
+            type="button"
+            className={`px-4 py-2 rounded-lg text-white  transition-all duration-200 ease-in-out nav-item
+            ${
+              pathname === "/queue"
+                ? "active"
+                : "text-opacity-30 hover:text-opacity-60"
+            }`}
+          >
+            Queue to play
+          </button>
+        </Link>
       </nav>
     </header>
   );
